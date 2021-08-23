7ef0ca5385cc31eabe0b157e31b4fb4f04d9e606
ivasy <ivasy@DESKTOP-KUURTQU>
2021-08-23 17:42:42 +0200

    ewer

--- src/main/java/com/aeClub/config/AEApplication.java
+++ src/main/java/com/aeClub/config/AEApplication.java
@@ -9,5 +9,6 @@ public class AEApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AEApplication.class, args);
+        //kfjgklsfdjgkldfjglkfdjglksdfj
     }
 }
