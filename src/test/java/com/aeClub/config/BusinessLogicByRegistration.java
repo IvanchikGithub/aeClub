c11004a65a32cca16a617afe10fa8cd8abaaa8fe
ivasy <ivasy@DESKTOP-KUURTQU>
2021-10-20 14:54:17 +0200

    AES-17
    UnitTests und IntegrationTests
    
    Die Nutzer mit zusaetztliche Information wird in Datenbank gespeichert

--- src/test/java/com/aeClub/config/BusinessLogicByRegistration.java
+++ src/test/java/com/aeClub/config/BusinessLogicByRegistration.java
@@ -1,10 +1,15 @@
 package com.aeClub.config;
 
+import static org.springframework.util.Assert.isInstanceOf;
+import static org.springframework.util.Assert.isTrue;
+
+
+
+import java.time.LocalDate;
 import java.util.stream.Stream;
 
 import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -16,11 +21,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.util.Assert;
 
 import com.aeClub.entity.Account;
+import com.aeClub.enums.AmmountChildrenType;
 import com.aeClub.enums.CountryList;
 import com.aeClub.enums.DenominationType;
+import com.aeClub.enums.EducationLevel;
 import com.aeClub.form.AccountForm;
 import com.aeClub.service.CreateNewUserService;
 import com.aeClub.service.FindService;
@@ -29,56 +35,124 @@ import com.aeClub.util.AccountEmpty;
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource("/application-test.properties")
-@TestMethodOrder (OrderAnnotation.class)
+@TestMethodOrder(OrderAnnotation.class)
 public class BusinessLogicByRegistration {
 	@Autowired
 	private CreateNewUserService createNewUserService;
 	@Autowired
 	private FindService findService;
 
+	private static final String EMAIL_1 = "qwe7@1.1";
+	private static final String EMAIL_2 = "qwe8@1.1";
+	private static final String NAME_USER_1 = "BeautifulSunTestName";
+	private static final String NAME_USER_2 = "HugePowerTestName";
+
 	@AfterAll
-	private static void deleting (@Autowired FindService findService) {
-		findService.findAndDeleteFromAccountTable("BeautifulSun");
-		findService.findAndDeleteFromEmailPassTable("qwe7@1.1");
-		findService.findAndDeleteFromEmailPassTable("qwe8@1.1");
+	private static void deleting(@Autowired FindService findService) {
+		findService.findAndDeleteFromAccountTable(NAME_USER_1);
+		findService.findAndDeleteFromAccountTable(NAME_USER_2);
+		findService.findAndDeleteFromEmailPassTable(EMAIL_1);
+		findService.findAndDeleteFromEmailPassTable(EMAIL_2);
 	}
-	
-		public static Stream<Object> dataForCreatingMainInfoUsers () {
+
+	@ParameterizedTest
+	@CsvSource(value = { "'qwe7@1.1', 'Bgt56yhN'", "'qwe8@1.1', 'Nhy67ujM'" })
+	@Order(1)
+	public void savingNewEmailAndPassInDataBase(String email, String pass) {
+		isTrue(!findService.isEmailRegistred(email), "email is not deleted");
+		createNewUserService.creatingNewPairEmailAndPass(email, pass);
+		isTrue(findService.isEmailRegistred(email), "email is not created");
+	}
+
+	@ParameterizedTest
+	@MethodSource("dataForCreatingUserWithMainInfoOnly")
+	@Order(2)
+	public void usersMainInfoIsSavingInDataBank(String email, AccountForm accountForm) {
+		int idUser = findService.giveMeIdUserForEmail(email);
+		if (idUser != 0) {
+			Account account = findService.getAccountById(idUser);
+			isInstanceOf(AccountEmpty.class, account, "account for " + email + "must not be null");
+			createNewUserService.createUsersMainInformation(idUser, accountForm, null, null);
+			account = findService.getAccountById(idUser);
+			
+			isInstanceOf(Account.class, account, "account is not saved");
+			isTrue(account.getNameForClub().equals(NAME_USER_1), "the name for club is not saved");
+			isTrue(account.getGender().equals("woman"), "the gender is not saved");
+			isTrue(account.getBirthdate().equals(LocalDate.of(1968, 12, 25)),
+					"the birthday is not saved");
+			isTrue(account.getCountry().equals(CountryList.TURKEY.getName()),
+					"the country is not saved");
+			isTrue(account.getCity().equals("Stambul"), "the city is not saved");
+			isTrue(account.getDenomination().equals(DenominationType.CHARISMATIC_CHURCH.getName()),
+					"the denomination is not saved");
+
+		} else {
+			throw new IllegalArgumentException("idUser must not be 0");
+		}
+	}
+
+	public static Stream<Object> dataForCreatingUserWithMainInfoOnly() {
 		AccountForm accountForm = new AccountForm();
-		accountForm.setNameForClub("BeautifulSun");
+		accountForm.setNameForClub(NAME_USER_1);
 		accountForm.setGender("woman");
 		accountForm.setBirthdateFromForm("1968-12-25");
 		accountForm.setCountry(CountryList.TURKEY.getName());
 		accountForm.setCity("Stambul");
 		accountForm.setDenomination(DenominationType.CHARISMATIC_CHURCH.getName());
-		Arguments[] testData = {Arguments.of(accountForm)};
+
+		Arguments[] testData = { Arguments.of(EMAIL_1, accountForm) };
+
 		return Arrays.asList(testData).stream();
 	}
-	
 
 	@ParameterizedTest
-	@CsvSource (value= {"'qwe7@1.1', 'Bgt56yhN'", "'qwe8@1.1', 'Nhy67ujM'"})
-	@Order(1)
-	public void savingNewEmailAndPassInDataBase(String email, String pass) {
-		Assert.isTrue(!findService.isEmailRegistred(email),"email is not deleted");
-		createNewUserService.creatingNewPairEmailAndPass(email, pass);
-		Assert.isTrue(findService.isEmailRegistred(email),"email is not created");
-	}
-	
+	@MethodSource("dataForCreatingUserWithMainInfoAndExtraInfo")
+	public void usersMainInfoAndExtraInfoIsSavedInDataBank(String email, AccountForm accountForm) {
+		int idUser = findService.giveMeIdUserForEmail(email);
+		if (idUser != 0) {
+			Account account = findService.getAccountById(idUser);
+			isInstanceOf(AccountEmpty.class, account, "account for " + email + "must not be null");
+			createNewUserService.createUsersMainInformation(idUser, accountForm, null, null);
+			account = findService.getAccountById(idUser);
+			isInstanceOf(Account.class, account, "account is not saved");
+			isTrue(account.getNameForClub().equals(NAME_USER_2), "the name for club is not saved");
+			isTrue(account.getGender().equals("man"), "the gender is not saved");
+			isTrue(account.getBirthdate().equals(LocalDate.of(1976, 04, 11)),
+					"the birthday is not saved");
+			isTrue(account.getCountry().equals(CountryList.BRAZIL.getName()),
+					"the country is not saved");
+			isTrue(account.getCity().equals("Rio"), "the city is not saved");
+			isTrue(account.getDenomination().equals(DenominationType.NOT_PROTESTANT_CHURCH.getName()),
+					"the denomination is not saved");
+			isTrue(account.getAccountExtraInfo().getRealName().equals("Piter"), "the real name is not saved");
+			isTrue(account.getAccountExtraInfo().getRealSurname().equals("Schmidt"), "the real name is not saved");
+			isTrue(account.getAccountExtraInfo().getAmountChildren().equals(AmmountChildrenType.NO_CHILDREN.getName()), "the real name is not saved");
+			isTrue(account.getAccountExtraInfo().getEducation().equals(EducationLevel.BACHELORS_DEGREE.getName()), "the real name is not saved");
+			isTrue(account.getAccountExtraInfo().getAboutMe().equals("I'm a good person"), "the real name is not saved");
+			isTrue(account.getAccountExtraInfo().getAboutYou().equals("I want to meet very good woman"), "the real name is not saved");
 
-	
-	@ParameterizedTest
-	@MethodSource("dataForCreatingMainInfoUsers")
-	@Order(2)
-	public void usersMainInfoIsSavingInDataBank(AccountForm accountForm) {
-		int idUser = findService.giveMeIdUserForEmail("qwe7@1.1");
-			if (idUser!=0) {
-				Account account = findService.getAccountById(idUser);
-				Assert.isInstanceOf(AccountEmpty.class, account, "account for \"qwe7@1.1\" not null");
-				createNewUserService.createUsersMainInformation(idUser, accountForm, null, null);
-				Assert.notNull(findService.getAccountById(idUser), "account is not saved");
-			}
+		} else {
+			throw new IllegalArgumentException("idUser must not be 0");
+		}
 	}
-	
+
+	public static Stream<Object> dataForCreatingUserWithMainInfoAndExtraInfo() {
+		AccountForm accountForm = new AccountForm();
+		accountForm.setNameForClub(NAME_USER_2);
+		accountForm.setGender("man");
+		accountForm.setBirthdateFromForm("1976-04-11");
+		accountForm.setCountry(CountryList.BRAZIL.getName());
+		accountForm.setCity("Rio");
+		accountForm.setDenomination(DenominationType.NOT_PROTESTANT_CHURCH.getName());
+		accountForm.setRealName("Piter");
+		accountForm.setRealSurname("Schmidt");
+		accountForm.setAmountChildren(AmmountChildrenType.NO_CHILDREN.getName());
+		accountForm.setEducation(EducationLevel.BACHELORS_DEGREE.getName());
+		accountForm.setAboutMe("I'm a good person");
+		accountForm.setAboutYou("I want to meet very good woman");
+
+		Arguments[] testData = { Arguments.of(EMAIL_2, accountForm) };
+		return Arrays.asList(testData).stream();
+	}
 
 }
